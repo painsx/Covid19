2b4fdd7abfcd09d8f901c7e1bf7d3797f725d72b
painsx <77559725+painsx@users.noreply.github.com>
2021-10-22 18:03:35 -0300

    Add files via upload

---
+++ COVID19.py
@@ -0,0 +1,12 @@
+# Don't work on Terminal Linux
+#Não funciona em terminais Linux
+'
+from turtle import *
+speed(10)
+color('cyan')
+bgcolor('black')
+b = 200
+while b > 0:
+	left(b)
+	forward(b * 1)
+	b = b - 1
